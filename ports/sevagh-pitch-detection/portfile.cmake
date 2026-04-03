07fc3015b3df6a5437254d7cce120d3d3f225840
Sergei Lebedev <limentas@gmail.com>
2026-04-03 14:39:10 +0200

    Fix find config module

--- ports/sevagh-pitch-detection/portfile.cmake
+++ ports/sevagh-pitch-detection/portfile.cmake
@@ -6,8 +6,9 @@ vcpkg_from_github(
     REF "407c693809f127f71b85848e36848f45a5a29213"
     SHA512 046420fdbcb669cb597b6a451b6c5c108235bee76ae8f57f9c8459761423e7f271ed79de540c83461d6d0d1cd664dd8dbfcf90ffcdeb9c58b89777d193afcbb9
     HEAD_REF master
-#    PATCHES
-#    fix-msvc-build.patch
+
+    # PATCHES
+    # fix-msvc-build.patch
 
     # fix-install.patch
     # remove-static-suffix.patch
@@ -24,16 +25,16 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-# configure_package_config_file(
-# "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in"
-# "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}/unofficial-${PORT}-config.cmake"
-# INSTALL_DESTINATION "share/${PORT}"
-# )
+configure_package_config_file(
+    "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/${PORT}-config.cmake"
+    INSTALL_DESTINATION "share/${PORT}"
+)
+
 # vcpkg_fixup_pkgconfig()
 
-#file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sevagh-pitch-detection")
-
-# file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+# file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sevagh-pitch-detection")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
