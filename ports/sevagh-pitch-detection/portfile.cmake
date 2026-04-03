881c6134d45ab6ec31ed204a61c5bff979317785
Sergei Lebedev <limentas@gmail.com>
2026-04-03 13:58:08 +0200

    Fix

--- ports/sevagh-pitch-detection/portfile.cmake
+++ ports/sevagh-pitch-detection/portfile.cmake
@@ -31,7 +31,7 @@ vcpkg_copy_pdbs()
 # )
 # vcpkg_fixup_pkgconfig()
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sevagh-pitch-detection")
+#file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sevagh-pitch-detection")
 
 # file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
